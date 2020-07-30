2fc1e7008652dc15c3744a931ddac2de1785f867
allemanfredi <alle.manfredi@gmail.com>
2020-07-31 00:22:55 +0200

    chore(global): adds configs for mainnet deployment

--- buidler.config.js
+++ buidler.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { usePlugin } = require('@nomiclabs/buidler/config')
 const hooks = require('./scripts/buidler-hooks')
 
@@ -6,18 +7,36 @@ usePlugin('@nomiclabs/buidler-solhint')
 usePlugin('buidler-gas-reporter')
 usePlugin('solidity-coverage')
 
+const getEnvironmentVariable = _envVar =>
+  process.env[_envVar]
+    ? process.env[_envVar]
+    : (
+      console.error(
+        '✘ Cannot migrate!',
+        '✘ Please provide an infura api key as and an',
+        '✘ account private key as environment variables:',
+        '✘ MAINNET_PRIVATE_KEY',
+        '✘ RINKEBY_PRIVATE_KEY',
+        '✘ INFURA_KEY'
+      ),
+      process.exit(1)
+    )
+
 module.exports = {
   defaultNetwork: 'localhost',
   networks: {
     localhost: {
       url: 'http://localhost:8545',
-      accounts: {
-        mnemonic: "explain tackle mirror kit van hammer degree position ginger unfair soup bonus"
-      }
     },
-    coverage: {
-      url: 'http://localhost:8555',
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${getEnvironmentVariable('INFURA_KEY')}`,
+      accounts: [getEnvironmentVariable('RINKEBY_PRIVATE_KEY')]
     },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${getEnvironmentVariable('INFURA_KEY')}`,
+      accounts: [getEnvironmentVariable('MAINNET_PRIVATE_KEY')],
+      gasPrice: 45e9
+    }
   },
   solc: {
     version: '0.4.24',
